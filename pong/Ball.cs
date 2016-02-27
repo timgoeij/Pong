b47b59bdbd114f28f8a1de56d3be7eb890b0ace2
timgoeij <tim_goeijenbier@live.nl>
2016-02-27 17:07:55 +0100

    first files of project

---
+++ pong/Ball.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace pong
+{
+    public class Ball
+    {
+        private Texture2D bal;
+        private Rectangle ballRect;
+        private Viewport view;
+
+        public Rectangle BallRect
+        {
+            get { return ballRect; }
+        }
+
+        private Vector2 pos;
+
+        public Vector2 Pos
+        {
+            get { return pos; }
+        }
+
+        private Vector2 velocity;
+
+        public Vector2 Velocity
+        {
+            get { return velocity; }
+            set { velocity = value; }
+        }
+        
+        public Ball(Viewport view)
+        {
+            this.view = view;
+            pos = new Vector2(view.Width / 2, view.Height / 2);
+        }
+
+        public void loadContent(Texture2D tex)
+        {
+            bal = tex;
+            ballRect = new Rectangle(view.Width / 2, view.Height / 2, tex.Width, tex.Height);
+
+            velocity = new Vector2(4.0f, 4.0f);
+        }
+
+        public void update(Paddle player, Paddle cpu)
+        {
+            if(pos.X < 0 || pos.X + bal.Width > view.Width)
+            {
+                reset();
+            }
+
+            if(pos.Y < 0 || pos.Y + bal.Height > view.Height)
+            {
+                velocity.Y *= -1;
+            }
+
+            checkCollision(player);
+            checkCollision(cpu);
+
+            pos += velocity;
+            
+            ballRect.X += (int)velocity.X;
+            ballRect.Y += (int)velocity.Y;
+        }
+
+        public void draw(SpriteBatch spritebatch)
+        {
+            spritebatch.Draw(bal, pos, Color.White);
+        }
+
+        private void reset()
+        {
+            pos = new Vector2(view.Width / 2, view.Height / 2);
+            ballRect.X = view.Width / 2;
+            ballRect.Y = view.Height / 2;
+
+            Random rndX = new Random(DateTime.Now.Millisecond);
+            Random rndY = new Random(DateTime.Now.Millisecond);
+
+            if (rndX.Next(0, 2) == 0)
+                velocity.X = -4;
+            else
+                velocity.X = 4;
+
+            if (rndY.Next(0, 2) == 0)
+                velocity.Y = -4;
+            else
+                velocity.Y = 4;
+        }
+
+        private void checkCollision(Paddle paddle)
+        {
+            if (ballRect.Intersects(paddle.PaddleRect))
+            {
+                if (pos.Y < paddle.PaddleRect.Top && pos.Y + ballRect.Height > paddle.PaddleRect.Top)
+                {
+                    velocity *= -1;
+                }
+                
+                if(pos.Y < paddle.PaddleRect.Bottom && pos.Y + ballRect.Height > paddle.PaddleRect.Bottom)
+                {
+                    velocity.Y *= -1;
+                }
+                
+                if(pos.X + ballRect.Width > paddle.PaddleRect.Left && pos.X < paddle.PaddleRect.Left)
+                {
+
+                    velocity.X *= -1;
+                }
+                
+                if(pos.X < paddle.PaddleRect.Right && pos.X + ballRect.Width > paddle.PaddleRect.Right)
+                {
+                    velocity.X *= -1;
+                }
+            }
+        }
+    }
+}
---
+++ pong/Paddle.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace pong
+{
+    public class Paddle
+    {
+        private Texture2D paddle;
+        private Vector2 pos;
+        private Rectangle paddleRect;
+        private Boolean cpu;
+        private Viewport view;
+
+        public Rectangle PaddleRect
+        {
+            get { return paddleRect; }   
+        }
+
+        public Vector2 Pos
+        {
+            get { return pos; }
+        }
+
+        public Paddle(Boolean cpu, Viewport view)
+        {
+            this.cpu = cpu;
+            this.view = view;
+
+            if(cpu)
+            {
+                pos = new Vector2(view.Width - 70, view.Height / 2);
+            }
+            else
+            {
+                pos = new Vector2(40, view.Height / 2);
+            }
+        }
+        
+        public void loadContent(Texture2D tex)
+        {
+            paddle = tex;
+
+            if(cpu)
+            {
+                paddleRect = new Rectangle(view.Width - 70, view.Height / 2, tex.Width, tex.Height);
+            }
+            else
+            {
+                paddleRect = new Rectangle(40, view.Height / 2, tex.Width, tex.Height);
+            }
+        }
+
+        public void cpuUpdate(Ball ball)
+        {
+            if(ball.Pos.Y + ball.BallRect.Height < pos.Y + (paddleRect.Height / 2))
+            {
+                if(pos.Y > 50)
+                {
+                    pos.Y -= 3;
+                    paddleRect.Y -= 3;
+                }
+            }
+
+            if(ball.Pos.Y > pos.Y + paddleRect.Height / 2)
+            {
+                if(pos.Y + paddleRect.Y < view.Height - 50)
+                {
+                    pos.Y += 3;
+                    paddleRect.Y += 3;
+                }
+            }
+        }
+
+        public void playerUpdate()
+        {
+            KeyboardState kbs = Keyboard.GetState();
+
+            if(kbs.IsKeyDown(Keys.Up) && pos.Y > 50)
+            {
+                pos.Y -= 3;
+                paddleRect.Y -= 3;
+            }
+
+            if(kbs.IsKeyDown(Keys.Down) && pos.Y + paddle.Height < view.Height - 50)
+            {
+                pos.Y += 3;
+                paddleRect.Y += 3;
+            }
+        }
+
+       
+
+        public void draw(SpriteBatch spritebatch)
+        {
+            spritebatch.Draw(paddle, pos, Color.White);
+        }
+    }
+}
